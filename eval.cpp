30528daf99f849a6dbeb10aeea1ede64fe8d0902
Saggy Man <36827987+Sazgr@users.noreply.github.com>
2022-08-21 07:59:37 -0700

    Create eval.cpp

---
+++ eval.cpp
@@ -0,0 +1,21 @@
+#include "eval.h"
+
+int middlegame[13][64]{};
+int endgame[13][64]{};
+
+void pst_init() {
+    for (int pc{0}; pc<6; ++pc) {
+        for (int sq{0}; sq<64; ++sq) {
+            middlegame[pc*2][sq] = -mg_value[pc] - mg_table[pc][sq ^ 56];
+            endgame[pc*2][sq] = -eg_value[pc] - eg_table[pc][sq ^ 56];
+        }
+        for (int sq{0}; sq<64; ++sq) {
+            middlegame[pc*2+1][sq] = mg_value[pc] + mg_table[pc][sq];
+            endgame[pc*2+1][sq] = eg_value[pc] + eg_table[pc][sq];
+        }
+    }
+    for (int sq{0}; sq<64; ++sq) {
+        middlegame[12][sq] = 0;
+        endgame[12][sq] = 0;
+    }
+}
