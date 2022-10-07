39dcacaea292f67e0c6ec9d159087c9e71e1e3bd
Saggy Man <36827987+Sazgr@users.noreply.github.com>
2022-10-07 18:21:09 -0400

    MSVC compatibility

--- bit_operations.h
+++ bit_operations.h
@@ -4,15 +4,27 @@
 #include "typedefs.h"
 
 inline int popcount(u64 bits) {
+#ifdef __GNUC__
     return __builtin_popcountll(bits);
+#elif _MSC_VER
+    return __popcnt64(bits);
+#endif
 }
 
 inline int get_lsb(u64 bits) {
+#ifdef __GNUC__
     return __builtin_ctzll(bits);
+#elif _MSC_VER
+    return _BitScanForward64(bits);
+#endif
 }
 
 inline int get_msb(u64 bits) {
+#ifdef __GNUC__
     return __builtin_clzll(bits) ^ 63;
+#elif _MSC_VER
+    return _BitScanReverse64(bits);
+#endif
 }
 
 inline int bitscan(u64 bits, bool reverse) {
@@ -24,7 +36,11 @@ inline int bitscan(u64 bits, bool reverse) {
 
 inline int pop_lsb(u64& bits) {
     int lsb = get_lsb(bits);
+#ifdef _MSC_VER
+    __blsr_u64(bits);
+#else
     bits &= bits - 1;
+#endif
     return lsb;
 }
 
