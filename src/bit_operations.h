72cf3fb03688ef96b608340d30ef5ee33fbb1ef1
Saggy Man <36827987+Sazgr@users.noreply.github.com>
2022-10-20 14:30:45 -0400

    Add support for C++20 Bit intrinsics

--- src/bit_operations.h
+++ src/bit_operations.h
@@ -3,45 +3,59 @@
 
 #include "typedefs.h"
 
-inline int popcount(u64 bits) {
 #ifdef __GNUC__
-    return __builtin_popcountll(bits);
-#elif _MSC_VER
-    return __popcnt64(bits);
-#endif
-}
 
-inline int get_lsb(u64 bits) {
-#ifdef __GNUC__
-    return __builtin_ctzll(bits);
-#elif _MSC_VER
-    return _BitScanForward64(bits);
-#endif
-}
-
-inline int get_msb(u64 bits) {
-#ifdef __GNUC__
-    return __builtin_clzll(bits) ^ 63;
-#elif _MSC_VER
-    return _BitScanReverse64(bits);
-#endif
-}
-
+inline int popcount(u64 bits) {return __builtin_popcountll(bits);}
+inline int get_lsb(u64 bits) {return __builtin_ctzll(bits);}
+inline int get_msb(u64 bits) {return __builtin_clzll(bits) ^ 63;}
 inline int bitscan(u64 bits, bool reverse) {
     u64 rmask;
     rmask = -u64(reverse);
     bits &= -bits | rmask;
     return get_msb(bits);
 }
-
 inline int pop_lsb(u64& bits) {
     int lsb = get_lsb(bits);
-#ifdef _MSC_VER
-    __blsr_u64(bits);
-#else
     bits &= bits - 1;
-#endif
     return lsb;
 }
+
+#elif _MSC_VER
+
+inline int popcount(u64 bits) {return __popcnt64(bits);}
+inline int get_lsb(u64 bits) {return _BitScanForward64(bits);}
+inline int get_msb(u64 bits) {return _BitScanReverse64(bits);}
+inline int bitscan(u64 bits, bool reverse) {
+    u64 rmask;
+    rmask = -u64(reverse);
+    bits &= -bits | rmask;
+    return get_msb(bits);
+}
+inline int pop_lsb(u64& bits) {
+    int lsb = get_lsb(bits);
+    __blsr_u64(bits);
+    return lsb;
+}
+
+#else
+
+#include <bit>
+
+inline int popcount(u64 bits) {return std::popcount(bits);}
+inline int get_lsb(u64 bits) {return countr_zero(bits);}
+inline int get_msb(u64 bits) {return countl_zero(bits) ^ 63;}
+inline int bitscan(u64 bits, bool reverse) {
+    u64 rmask;
+    rmask = -u64(reverse);
+    bits &= -bits | rmask;
+    return get_msb(bits);
+}
+inline int pop_lsb(u64& bits) {
+    int lsb = get_lsb(bits);
+    bits &= bits - 1;
+    return lsb;
+}
+
+#endif
 
 #endif
