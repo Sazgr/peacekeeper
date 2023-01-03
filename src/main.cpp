912c432bfad8422157314b279c912abeb11b35e1
Saggy Man <36827987+Sazgr@users.noreply.github.com>
2023-01-03 13:43:45 -0500

    Change copyright year

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 /*
 Peacekeeper Chess Engine
-    Copyright (C) 2022  Kyle Zhang
+    Copyright (C) 2022-2023  Kyle Zhang
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
