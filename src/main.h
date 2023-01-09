1c22415f02cd2af8141e08da6d190a18f246413a
Saggy Man <36827987+Sazgr@users.noreply.github.com>
2023-01-09 15:22:50 -0500

    Add static null move pruning and modify bounds

--- src/main.h
+++ src/main.h
@@ -9,15 +9,16 @@
 #include <cmath>
 
 enum Features : bool {
-    check_extensions     = false,
-    delta_pruning        = false,
-    futility_pruning     = false,
-    history_heuristic    = false,
-    late_move_reductions = false,
-    null_move_pruning    = false,
+    null_move_pruning    = true,
+    static_null_move     = true,
+    history_heuristic    = true,
+    futility_pruning     = true,
+    delta_pruning        = true,
+    late_move_reductions = true,
+    check_extensions     = true,
 };
 
-std::array<int, 4> futile_margins{120, 150, 200, 270};
+std::array<int, 4> futile_margins{145, 205, 251, 290};
 
 u64 perft(Position& position, int depth);
 template <bool side> u64 perft_f(Position& position, int depth);
@@ -28,6 +29,9 @@ void iterative_deepening(Position& position, Stop_timer& timer, Hashtable& table
 inline int lmr_reduction(bool is_pv, int depth, int move_num) {
     if (is_pv) return static_cast<int>((std::log(move_num - 2) * std::log(depth) + 1.44) / 4.1);
     else return static_cast<int>((std::log(move_num - 2) * std::log(depth) + 0.9) / 1.9);
+}
+inline bool no_mate(int alpha, int beta) {
+    return (-18000 < alpha) && (beta < 18000);
 }
 
 #endif
