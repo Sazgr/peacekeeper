d51dc581dc0ac688ba9cfa61b772894fea9530a0
Saggy Man <36827987+Sazgr@users.noreply.github.com>
2023-03-27 12:12:26 -0400

    Add relative history heuristic

--- src/history.h
+++ src/history.h
@@ -2,28 +2,35 @@
 #define PEACEKEEPER_HISTORY
 
 struct History_table {
-    int table[12][64]{};
-    int sum{};
-    void edit(int piece, int to_square, int change) {
-        table[piece][to_square] += change;
-        sum += change;
-        if (sum >= 0xFFFFFF || sum <= -0xFFFFFF) age();
+    int successes[12][64]{};
+    int all[12][64]{};
+    void edit(int piece, int to_square, int change, bool success) {
+        all[piece][to_square] += change;
+        if (success) successes[piece][to_square] += change << 12;
+        if (all[piece][to_square] > 0x3FFFF) {
+            all[piece][to_square] /= 2;
+            successes[piece][to_square] /= 2;
+        }
+    }
+    int value(int piece, int to_square) {
+        if (!all[piece][to_square]) return (1 << 10);
+        return successes[piece][to_square] / all[piece][to_square]; //ranges from 0 to 4095
     }
     void age() {
         for (int i{}; i<12; ++i) {
             for (int j{}; j<64; ++j) {
-                table[i][j] /= 2;
+                all[i][j] /= 2;
+                successes[i][j] /= 2;
             }
         }
-        sum /= 2;
     }
     void reset() {
         for (int i{}; i<12; ++i) {
             for (int j{}; j<64; ++j) {
-                table[i][j] = 0;
+                all[i][j] = 0;
+                successes[i][j] = 0;
             }
         }
-        sum = 0;
     }
 };
 
