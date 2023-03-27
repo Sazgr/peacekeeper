99c56b37912bc55e632cfa77eaf56375036e6a00
Saggy Man <36827987+Sazgr@users.noreply.github.com>
2023-03-27 12:27:19 -0400

    Add killers

--- src/main.h
+++ src/main.h
@@ -2,6 +2,7 @@
 #define PEACEKEEPER_MAIN
 
 #include "board.h"
+#include "killers.h"
 #include "hash.h"
 #include "history.h"
 #include "timer.h"
@@ -11,6 +12,7 @@
 enum Features : bool {
     null_move_pruning    = true,
     static_null_move     = true,
+    killer_heuristic     = true,
     history_heuristic    = true,
     futility_pruning     = true,
     delta_pruning        = true,
@@ -25,8 +27,8 @@ u64 perft(Position& position, int depth);
 template <bool side> u64 perft_f(Position& position, int depth);
 u64 perft_split(Position& position, int depth, std::vector<std::pair<Move, int>>& list);
 int quiescence(Position& position, Stop_timer& timer, int ply, int alpha, int beta);
-int pvs(Position& position, Stop_timer& timer, Hashtable& table, History_table& history, int depth, int ply, int alpha, int beta, bool is_pv, bool can_null);
-void iterative_deepening(Position& position, Stop_timer& timer, Hashtable& table, History_table& history, Move& bestmove);
+int pvs(Position& position, Stop_timer& timer, Hashtable& table, History_table& history, Killer_table& killer, int depth, int ply, int alpha, int beta, bool is_pv, bool can_null);
+void iterative_deepening(Position& position, Stop_timer& timer, Hashtable& table, History_table& history, Killer_table& killer, Move& bestmove);
 inline int lmr_reduction(bool is_pv, int depth, int move_num) {
     if (is_pv) return static_cast<int>((std::log(move_num - 3) * std::log(depth)) / 4.3 + 0.5);
     else return static_cast<int>((std::log(move_num - 3) * std::log(depth)) / 1.85 + 0.5);
