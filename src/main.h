e06b6ba9f83f594811a080aef73c02c7ed745fc6
Saggy Man <36827987+Sazgr@users.noreply.github.com>
2023-05-02 13:16:16 -0400

    Add OpenBench compliance

--- src/main.h
+++ src/main.h
@@ -30,7 +30,7 @@ u64 perft(Position& position, int depth);
 template <bool side> u64 perft_f(Position& position, int depth);
 u64 perft_split(Position& position, int depth, std::vector<std::pair<Move, int>>& list);
 int quiescence(Position& position, Stop_timer& timer, int ply, int alpha, int beta);
-int pvs(Position& position, Stop_timer& timer, Hashtable& table, History_table& history, Killer_table& killer, int depth, int ply, int alpha, int beta, bool is_pv, bool can_null);
+int pvs(Position& position, Stop_timer& timer, Hashtable& table, History_table& history, Killer_table& killer, int depth, int ply, int alpha, int beta, bool is_pv, bool can_null, bool output);
 void iterative_deepening(Position& position, Stop_timer& timer, Hashtable& table, History_table& history, Killer_table& killer, Move& bestmove);
 inline int lmr_reduction(bool is_pv, int depth, int move_num) {
     if (is_pv) return static_cast<int>((std::log(move_num - 3) * std::log(depth)) / 4.3 + 0.5);
