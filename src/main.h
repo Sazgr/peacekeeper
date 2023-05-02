42b959fcbaaa7a8ed621aefbcb567b84fc940399
Kyle Zhang <kyleyzhang@gmail.com>
2023-05-02 13:38:25 -0400

    Fix mistake

--- src/main.h
+++ src/main.h
@@ -30,8 +30,8 @@ u64 perft(Position& position, int depth);
 template <bool side> u64 perft_f(Position& position, int depth);
 u64 perft_split(Position& position, int depth, std::vector<std::pair<Move, int>>& list);
 int quiescence(Position& position, Stop_timer& timer, int ply, int alpha, int beta);
-int pvs(Position& position, Stop_timer& timer, Hashtable& table, History_table& history, Killer_table& killer, int depth, int ply, int alpha, int beta, bool is_pv, bool can_null, bool output);
-void iterative_deepening(Position& position, Stop_timer& timer, Hashtable& table, History_table& history, Killer_table& killer, Move& bestmove);
+int pvs(Position& position, Stop_timer& timer, Hashtable& table, History_table& history, Killer_table& killer, int depth, int ply, int alpha, int beta, bool is_pv, bool can_null);
+void iterative_deepening(Position& position, Stop_timer& timer, Hashtable& table, History_table& history, Killer_table& killer, Move& bestmove, bool output);
 inline int lmr_reduction(bool is_pv, int depth, int move_num) {
     if (is_pv) return static_cast<int>((std::log(move_num - 3) * std::log(depth)) / 4.3 + 0.5);
     else return static_cast<int>((std::log(move_num - 3) * std::log(depth)) / 1.85 + 0.5);
