c7b0895f9bccc85d9dc9d443d56eee991f2524d5
Kyle Zhang <kyleyzhang@gmail.com>
2023-07-31 12:25:02 -0400

    Clamp LMR to never go into quiescence

--- src/main.cpp
+++ src/main.cpp
@@ -688,6 +688,7 @@ int pvs(Position& position, Stop_timer& timer, Hashtable& table, Move_order_tabl
         if constexpr (late_move_reductions) if (depth > 2 && move_num >= 4 && !in_check && !gives_check) {
             reduce_this = lmr_reduction(is_pv, depth, move_num);
             if (movelist[i].sortkey() < 1500) ++reduce_this;
+            reduce_this = std::clamp(reduce_this, 0, depth - reduce_all - 1);
         }
         if (move_num == 1) {
             result = -pvs(position, timer, table, move_order, depth - reduce_all, -beta, -alpha, ss + 1);
