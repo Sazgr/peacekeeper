366a64a49e50024df1ca4d39460808bf84180b25
Kyle Zhang <kyleyzhang@gmail.com>
2023-08-31 09:18:09 -0400

    Fix FRC

--- src/main.cpp
+++ src/main.cpp
@@ -212,6 +212,10 @@ int main(int argc, char *argv[]) {
         if (tokens[0] == "setoption" && tokens[1] == "name") {
             if (tokens.size() >= 5 && tokens[2] == "Hash" && tokens[3] == "value") {hash.resize(stoi(tokens[4]));}
             if (tokens.size() >= 6 && tokens[2] == "Move" && tokens[3] == "Overhead" && tokens[4] == "value") {move_overhead = stoi(tokens[5]);}
+            if (tokens.size() >= 5 && tokens[2] == "UCI_Chess960" && tokens[3] == "value") {
+                if (tokens[5] == "true") chess960 = true;
+                if (tokens[5] == "false") chess960 = false;
+            }
 #ifdef SPSA
             if (tokens.size() >= 5 && tokens[2] == "futility_multiplier" && tokens[3] == "value") {
                 futility_multiplier = 0.1 * stoi(tokens[4]);
