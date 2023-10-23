5a47dbdf7b26f14d4d1e8445bc5f6d3f09fd60d1
Kyle Zhang <kyleyzhang@gmail.com>
2023-10-23 19:57:08 -0400

    Randomly try this

--- src/main.h
+++ src/main.h
@@ -34,7 +34,7 @@ enum Features : bool {
 
 spsa std::array<int, 6> futile_margins{47, 75, 98, 119, 138, 156};
 constexpr std::array<int, 3> aspiration_bounds{28, 90, 280};
-spsa std::array<double, 4> tc_stability{2.05, 1.20, 0.90, 0.85};
+spsa std::array<double, 4> tc_stability{2.44, 1.31, 0.84, 1.15};
 
 spsa double futility_multiplier = 47.5;
 spsa double futility_power = 0.666;
@@ -50,7 +50,7 @@ spsa double see_noisy_quadratic = 8.0;
 spsa double see_quiet_constant = 90.0;
 spsa double see_quiet_linear = 10.0;
 spsa double see_quiet_quadratic = 8.8;
-spsa double node_timescale_base = 2.1;
+spsa double node_timescale_base = 1.9;
 spsa double node_timescale_div = 2.1;
 spsa double aspiration_beta_timescale = 1.35;
 
