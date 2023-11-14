ea67f11a7d66fac8f5993501540a9dddc2dd550c
Kyle Zhang <kyleyzhang@gmail.com>
2023-11-13 19:27:23 -0500

    Hash replacement scheme

--- src/hash.h
+++ src/hash.h
@@ -118,7 +118,7 @@ public:
         if (bestmove.is_null() && previous.full_hash == hash) {
             bestmove = previous.bestmove;
         }
-        if (previous.depth <= dp + 3) table[hash & (size - 1)] = Packed_element{hash, bestmove, score, type, dp, table_age, ply};
+        if (previous.depth + (previous.type == tt_exact) <= dp + (type == tt_exact) + 3) table[hash & (size - 1)] = Packed_element{hash, bestmove, score, type, dp, table_age, ply};
     }
 private:
     std::vector<Packed_element> table;
