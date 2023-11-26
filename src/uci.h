f924a7c80cc5a200bbc3708ea2b2456afe2e015a
Kyle Zhang <kyleyzhang@gmail.com>
2023-11-26 16:13:49 -0500

    Less aggressive futility

--- src/uci.h
+++ src/uci.h
@@ -51,8 +51,8 @@ inline void print_info(std::ostream& out) {
         << "option name UCI_Chess960 type check default false" << '\n'
         << "option name EvalFile type string default <internal>" << '\n'
 #ifdef SPSA
-        << "option name futility_multiplier type spin default 400 min 0 max 1000" << '\n'
-        << "option name futility_power type spin default 70 min 0 max 1000" << '\n'
+        << "option name futility_multiplier type spin default 650 min 0 max 1000" << '\n'
+        << "option name futility_power type spin default 66 min 0 max 1000" << '\n'
         << "option name lmr_base type spin default 50 min 0 max 10000" << '\n'
         << "option name lmr_nopv_divisor type spin default 200 min 0 max 10000" << '\n'
         << "option name lmr_ispv_divisor type spin default 400 min 0 max 10000" << '\n'
