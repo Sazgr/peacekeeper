98e633542094b3751e779304b17834dad911c106
Kyle Zhang <kyleyzhang@gmail.com>
2024-01-01 12:06:22 -0500

    Remove handrolled SIMD

--- src/nnue.cpp
+++ src/nnue.cpp
@@ -1,24 +1,16 @@
 #include "board.h"
 #include "bit_operations.h"
 #include "nnue.h"
-#include "simd.h"
 #include <fstream>
 
 #define INCBIN_STYLE INCBIN_STYLE_SNAKE
 #include "incbin.h"
 INCBIN(eval, NETWORK_FILE);
 
-#ifdef SIMD
-alignas(ALIGNMENT) std::array<i16, input_size * hidden_size> input_weights;
-alignas(ALIGNMENT) std::array<i16, hidden_size> input_bias;
-alignas(ALIGNMENT) std::array<i16, hidden_dsize> hidden_weights;
-alignas(ALIGNMENT) std::array<i32, output_size> hidden_bias;
-#else
 std::array<i16, input_size * hidden_size> input_weights;
 std::array<i16, hidden_size> input_bias;
 std::array<i16, hidden_dsize> hidden_weights;
 std::array<i32, output_size> hidden_bias;
-#endif
 
 template void NNUE::update_accumulator<true>(int piece, int square, int black_king_square, int white_king_square);
 template void NNUE::update_accumulator<false>(int piece, int square, int black_king_square, int white_king_square);
@@ -28,20 +20,6 @@ template <bool add> void NNUE::update_accumulator(int piece, int square, int bla
     Accumulator& accumulator = accumulator_stack[current_accumulator];
     for (int side{}; side < 2; ++side) {
         const int inputs = index(piece, square, side, side ? white_king_square : black_king_square);
-#ifdef SIMD
-        const register_type* weights = reinterpret_cast<register_type*>(input_weights.data() + inputs * hidden_size);
-        const register_type* input = reinterpret_cast<register_type*>(accumulator[side].data());
-        register_type* output = reinterpret_cast<register_type*>(accumulator[side].data());
-        if constexpr (add) {
-            for (int i = 0; i < hidden_size / I16_STRIDE; ++i) {
-                output[i] = register_add_16(input[i], weights[i]);
-            }
-        } else {
-            for (int i = 0; i < hidden_size / I16_STRIDE; ++i) {
-                output[i] = register_sub_16(input[i], weights[i]);
-            }
-        }
-#else
         const i16* weights = input_weights.data() + inputs * hidden_size;
         if constexpr (add) {
             for (int i = 0; i < hidden_size; ++i) {
@@ -52,7 +30,6 @@ template <bool add> void NNUE::update_accumulator(int piece, int square, int bla
                 accumulator[side][i] -= weights[i];
             }
         }
-#endif
     }
 }
 
@@ -71,21 +48,6 @@ void NNUE::refresh(Position& position) {
 
 i32 NNUE::evaluate(bool side) {
     Accumulator &accumulator = accumulator_stack[current_accumulator];
-#ifdef SIMD
-    const register_type crelu_min{};
-    const register_type crelu_max = register_set_16(255);
-    register_type res{};
-    const register_type* accumulator_us = reinterpret_cast<register_type*>(accumulator[side].data());
-    const register_type* accumulator_them = reinterpret_cast<register_type*>(accumulator[!side].data());
-    const register_type* weights = reinterpret_cast<register_type*>(hidden_weights.data());
-    for (int i = 0; i < hidden_size / I16_STRIDE; ++i) {
-        res = register_add_32(res, register_madd_16(register_min_16(register_max_16(accumulator_us[i], crelu_min), crelu_max), weights[i]));
-    }
-    for (int i = 0; i < hidden_size / I16_STRIDE; ++i) {
-        res = register_add_32(res, register_madd_16(register_min_16(register_max_16(accumulator_them[i], crelu_min), crelu_max), weights[i + hidden_size / I16_STRIDE]));
-    }
-    i32 output = register_sum_32(res) + hidden_bias[0];
-#else
     i32 output = hidden_bias[0];
     for (int i = 0; i < hidden_size; ++i) {
         output += crelu(accumulator[side][i]) * hidden_weights[i];
@@ -93,7 +55,6 @@ i32 NNUE::evaluate(bool side) {
     for (int i = 0; i < hidden_size; ++i) {
         output += crelu(accumulator[!side][i]) * hidden_weights[hidden_size + i];
     }
-#endif
     return (output * 400) / input_quantization / hidden_quantization;
 }
 
