85549da2c41418fa7ed16244af1d79ae2f637735
Kyle Zhang <kyleyzhang@gmail.com>
2024-01-19 10:22:25 -0500

    Negative extension

--- src/main.cpp
+++ src/main.cpp
@@ -667,6 +667,8 @@ int pvs(Position& position, Stop_timer& timer, Hashtable& table, Move_order_tabl
                 return singular_beta;
             } else if (entry.score >= beta) {
                 extend_this = -1;
+            } else if (entry.score <= alpha && cutnode) {
+                extend_this = -1;
             }
         }
         position.make_move<true>(hash_move, sd.nnue);
