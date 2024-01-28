2240389e40f855ef82e0b9373219296d1ba48468
Kyle Zhang <kyleyzhang@gmail.com>
2024-01-28 17:28:35 -0500

    More IIR in special case

--- src/main.cpp
+++ src/main.cpp
@@ -645,7 +645,7 @@ int pvs(Position& position, Stop_timer& timer, Hashtable& table, Move_order_tabl
     if constexpr (check_extensions) if (in_check) {reduce_all -= 1;} //check extension
     Move hash_move = entry.bestmove;
     bool hash_move_usable = entry.type != tt_none && entry.full_hash == position.hashkey() && !hash_move.is_null() && position.board[hash_move.start()] == hash_move.piece();
-    if constexpr (internal_iterative_reduction) if (depth >= 6 && !hash_move_usable) reduce_all += 1;
+    if constexpr (internal_iterative_reduction) if (depth >= 6 && !hash_move_usable) reduce_all += 1 + (entry.type != tt_none && entry.full_hash == position.hashkey() && entry.depth >= depth);
     //Stage 1 - Hash Move
     if (hash_move_usable && hash_move != ss->excluded) {//searching best move from hashtable
         int extend_this = 0;
