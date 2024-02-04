b5888f6a0753416d47e2d7722ae295f833e124d7
Kyle Zhang <kyleyzhang@gmail.com>
2024-02-04 18:49:30 -0500

    No futility

--- src/main.h
+++ src/main.h
@@ -20,7 +20,7 @@ enum Features : bool {
     static_null_move     = true,
     killer_heuristic     = true,
     history_heuristic    = true,
-    futility_pruning     = true,
+    futility_pruning     = false,
 #ifdef DATAGEN
     late_move_pruning    = false,
 #else
