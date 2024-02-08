563674c227e618c8cac7204f1dfb3dd08834e645
Kyle Zhang <kyleyzhang@gmail.com>
2024-02-08 18:59:34 -0500

    Tweak singular search depth criteria

--- src/main.cpp
+++ src/main.cpp
@@ -649,7 +649,7 @@ int pvs(Position& position, Stop_timer& timer, Hashtable& table, Move_order_tabl
     //Stage 1 - Hash Move
     if (hash_move_usable && hash_move != ss->excluded) {//searching best move from hashtable
         int extend_this = 0;
-        if (!is_root && depth >= (6 + is_pv) && (entry.type == tt_exact || entry.type == tt_beta) && no_mate(entry.score, entry.score) && entry.depth >= depth - 3) {
+        if (!is_root && depth >= (6 + is_pv) && (entry.type == tt_exact || entry.type == tt_beta) && no_mate(entry.score, entry.score) && entry.depth >= depth - 4) {
             int singular_beta = entry.score - depth * singular_extension_margin / 16;
             int singular_depth = (depth - 1) / 2;
             ss->excluded = hash_move;
