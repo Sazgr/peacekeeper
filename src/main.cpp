2239b3a4d22b9fceca863824edd717a16f679517
Kyle Zhang <kyleyzhang@gmail.com>
2024-02-09 14:12:42 -0500

    QS SEE change

--- src/main.cpp
+++ src/main.cpp
@@ -572,7 +572,7 @@ int quiescence(Position& position, Stop_timer& timer, Hashtable& table, int alph
         for (int i = 0; i < movelist.size(); ++i) movelist[i].add_sortkey(movelist[i].mvv_lva());
         movelist.sort(0, movelist.size());
         for (int i = 0; i < movelist.size(); ++i) {
-            if (!see(position, movelist[i], -274)) continue;
+            if (!see(position, movelist[i], -1)) continue;
             position.make_move<true>(movelist[i], sd.nnue);
             ss->move = movelist[i];
             ++sd.nodes;
