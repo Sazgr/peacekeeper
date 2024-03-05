7e1209761d6d8e5bceda0f994199ffb37bfaf233
Kyle Zhang <kyleyzhang@gmail.com>
2024-03-05 08:09:29 -0500

    Triple extensions

--- src/main.cpp
+++ src/main.cpp
@@ -739,7 +739,7 @@ int pvs(Position& position, Stop_timer& timer, Hashtable& table, Move_order_tabl
                 ss->excluded = Move{};
                 if (singular_score < singular_beta) {
                     if (!is_pv && singular_score < singular_beta - double_extension_margin && ss->double_extensions <= 4) {
-                        extend_this = 2;
+                        extend_this = 2 + (hash_move.captured() == 12 && singular_score < singular_beta - 250);
                         ++ss->double_extensions;
                     } else {
                         extend_this = 1;
