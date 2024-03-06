51c9de73cc5269d651efb7620e637f9461994ab2
Kyle Zhang <kyleyzhang@gmail.com>
2024-03-06 18:02:42 -0500

    Less check extensions

--- src/main.cpp
+++ src/main.cpp
@@ -686,7 +686,7 @@ int pvs(Position& position, Stop_timer& timer, Hashtable& table, Move_order_tabl
             }
         }
     }
-    if constexpr (check_extensions) if (in_check) {reduce_all -= 1;} //check extension
+    if constexpr (check_extensions) if (depth >= 4 && in_check) {reduce_all -= 1;} //check extension
     if constexpr (internal_iterative_reduction) if (depth >= 6 && !hash_move_usable) reduce_all += 1;
     Movelist movelist;
     for (int stage = stage_hash_move; stage != stage_finished; ++stage) { //generating and sorting one stage
