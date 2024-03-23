9d8b1518ef0cf1ee607e65fc1381ca0b9c3ee370
Kyle Zhang <kyleyzhang@gmail.com>
2024-03-23 16:44:44 -0400

    Butterfly history

--- src/main.cpp
+++ src/main.cpp
@@ -714,7 +714,7 @@ int pvs(Position& position, Stop_timer& timer, Hashtable& table, Move_order_tabl
                 for (int i = 0; i < movelist.size(); ++i) {
                     int score{};
                     if constexpr (history_heuristic) {
-                        score += move_order.history_value(movelist[i]);
+                        score += std::min(move_order.history_value(movelist[i]), move_order.butterfly_value(movelist[i]));
                         score += move_order.continuation_value((ss - 2)->move, movelist[i]);
                         score += move_order.continuation_value((ss - 1)->move, movelist[i]);
                     }
@@ -810,11 +810,13 @@ int pvs(Position& position, Stop_timer& timer, Hashtable& table, Move_order_tabl
                         }
                         if constexpr (history_heuristic) if (stage == stage_quiet) for (int j{0}; j<i; ++j) {
                             move_order.history_edit(movelist[j], history_bonus(depth), false);
+                            move_order.butterfly_edit(movelist[j], history_bonus(depth), false);
                             move_order.continuation_edit((ss - 2)->move, movelist[j], history_bonus(depth), false);
                             move_order.continuation_edit((ss - 1)->move, movelist[j], history_bonus(depth), false);
                         }
                         if constexpr (history_heuristic) if (bestmove.captured() == 12) {
                             move_order.history_edit(bestmove, history_bonus(depth), true);
+                            move_order.butterfly_edit(bestmove, history_bonus(depth), true);
                             move_order.continuation_edit((ss - 2)->move, bestmove, history_bonus(depth), true);
                             move_order.continuation_edit((ss - 1)->move, bestmove, history_bonus(depth), true);
                         }
