cd04e1e83f76fed0086f2c0bae2e34b3f8220c2b
Kyle Zhang <kyleyzhang@gmail.com>
2024-04-04 10:25:20 -0400

    Fail medium razoring

--- src/main.cpp
+++ src/main.cpp
@@ -657,7 +657,7 @@ int pvs(Position& position, Stop_timer& timer, Hashtable& table, Move_order_tabl
         }
     }
     if constexpr (razoring) if (depth < 4 && !is_pv && !in_check && ss->excluded.is_null() && static_eval + 30 + 90 * depth * depth <= alpha) {
-        return quiescence(position, timer, table, alpha, beta, ss, sd);
+        return (quiescence(position, timer, table, alpha, beta, ss, sd) + alpha) / 2;
     }
     if constexpr (probcut) {
         int probcut_beta = beta + probcut_margin;
